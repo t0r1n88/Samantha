1e22639057367717bbaeb5eb8ceb168a7d3c63da
Oleg Budaev <t0r1n88@mail.ru>
2022-03-15 09:05:54 +0800

    Реалихован графический интерфейс. Программа готова к работе

--- Samantha_GUI.py
+++ Samantha_GUI.py
@@ -13,6 +13,8 @@ from datetime import date
 from openpyxl.chart.label import DataLabelList
 from openpyxl.chart import BarChart, Reference, PieChart, PieChart3D, Series
 import warnings
+from openpyxl.styles import Font
+from openpyxl.styles import Alignment
 
 
 def resource_path(relative_path):
@@ -43,8 +45,92 @@ def select_end_folder_calculate_budget_spo():
     path_to_end_folder_calculate_budget_spo = filedialog.askdirectory()
 
 def calculate_budget_spo():
-    pass
+    # получаем список листов в таблице
+    try:
+        sheetnames = openpyxl.load_workbook(name_file_data_calculate_budget_spo).sheetnames
 
+        # Создаем документ openpyxl
+
+        wb = openpyxl.Workbook()
+        wb.create_sheet(title='Сводная таблица', index=0)
+        wb.create_sheet(title='ГАПОУ', index=1)
+        wb.create_sheet(title='ГБПОУ', index=2)
+
+        # получаем базовый датафрейм
+        base_df = pd.read_excel(name_file_data_calculate_budget_spo, sheet_name='СВОД')
+
+        # Итерируемся открываем листы
+        for sheet in sheetnames:
+            if sheet != 'СВОД':
+                # Считываем датафрейм
+                temp_df = pd.read_excel(name_file_data_calculate_budget_spo, sheet_name=sheet)
+                base_df = pd.concat([base_df, temp_df], axis=0, ignore_index=True)
+
+        clean_df = clean_data(base_df)
+
+        #
+        # Разделяем по ГАПОУ и ГБПОУ
+        g_df = clean_df.copy()
+
+        group_df = g_df.groupby(['Наименование учреждения'], as_index=False).agg({'Сумма, тыс. рублей': 'sum'})
+
+        group_df['Тип'] = group_df['Наименование учреждения'].apply(lambda x: 'ГАПОУ' if 'ГАПОУ' in x else 'ГБПОУ')
+
+        ga_df = group_df[group_df['Тип'] == 'ГАПОУ']
+        gb_df = group_df[group_df['Тип'] == 'ГБПОУ']
+
+        # Удаляем колонки  с типом
+        out_ga_df = ga_df.drop(columns='Тип')
+        out_gb_df = gb_df.drop(columns='Тип')
+
+        # Записываем результаты в соответствующие листы
+        for r in dataframe_to_rows(clean_df, index=False, header=True):
+            if len(r) != 1:
+                wb['Сводная таблица'].append(r)
+
+        for r in dataframe_to_rows(out_ga_df, index=False, header=True):
+            if len(r) != 1:
+                wb['ГАПОУ'].append(r)
+
+        for r in dataframe_to_rows(out_gb_df, index=False, header=True):
+            if len(r) != 1:
+                wb['ГБПОУ'].append(r)
+
+        # Устанавливаем ширину колонок
+        wb['Сводная таблица'].column_dimensions['A'].width = 5
+        wb['Сводная таблица'].column_dimensions['B'].width = 50
+
+        wb['Сводная таблица'].column_dimensions['C'].width = 50
+        wb['Сводная таблица'].column_dimensions['D'].width = 20
+        wb['Сводная таблица'].column_dimensions['E'].width = 50
+        wb['Сводная таблица'].column_dimensions['F'].width = 50
+
+        wb['ГАПОУ'].column_dimensions['A'].width = 50
+        wb['ГБПОУ'].column_dimensions['A'].width = 50
+
+        # Получаем текущее время для того чтобы использовать в названии
+        t = time.localtime()
+        current_time = time.strftime('%H_%M_%S', t)
+        # Сохраняем итоговый файл
+        wb.save(f'{path_to_end_folder_calculate_budget_spo}/Бюджетная заявка от СПО.xlsx {current_time}.xlsx')
+    except NameError:
+        messagebox.showinfo('ЦОПП Бурятия', f'Выберите файл с данными и папку куда будут генерироваться файлы')
+
+    else:
+        messagebox.showinfo('ЦОПП Бурятия','Подсчет успешно завершен!')
+
+def clean_data(raw_df:pd.DataFrame) ->pd.DataFrame:
+    raw_df['Сумма, тыс. рублей'].fillna('0.0', inplace=True)
+    # base_df.to_excel('Общий.xlsx',index=False)
+    # Очищаем столбец от пробелов
+    raw_df['Сумма, тыс. рублей'] = raw_df['Сумма, тыс. рублей'].astype('str')
+
+    raw_df['Сумма, тыс. рублей'] = raw_df['Сумма, тыс. рублей'].apply(lambda x: x.replace(' ', ''))
+    # Меняем запятую на точку
+    raw_df['Сумма, тыс. рублей'] = raw_df['Сумма, тыс. рублей'].apply(lambda x: x.replace(',', '.'))
+
+    raw_df['Сумма, тыс. рублей'] = raw_df['Сумма, тыс. рублей'].astype('float')
+    return raw_df
 
 
 if __name__=='__main__':
@@ -89,7 +175,7 @@ if __name__=='__main__':
     #
     # Создаем кнопку для выбора папки куда будет генерироваться файл
 
-    btn_choose_end_folder_doc = Button(frame_data_for_calculate_budget_spo, text='3) Выберите конечную папку', font=('Arial Bold', 20),
+    btn_choose_end_folder_doc = Button(frame_data_for_calculate_budget_spo, text='2) Выберите конечную папку', font=('Arial Bold', 20),
                                        command=select_end_folder_calculate_budget_spo
                                        )
     btn_choose_end_folder_doc.grid(column=0, row=4, padx=10, pady=10)
